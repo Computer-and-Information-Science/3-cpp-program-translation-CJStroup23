be82188f2d7a2e9a680e95f32e3603a7efe02d4a
CJStroup23 <138122479+CJStroup23@users.noreply.github.com>
2023-09-23 17:40:47 -0400

    Update assembly.asm

--- assembly.asm
+++ assembly.asm
@@ -1,5 +1,5 @@
 ; CISC 225 - #3 C++ Program Translation
-; YOUR NAME HERE
+; Colton Stroup
 
 global _start		; exposes program entry point to the linker
 
@@ -8,28 +8,48 @@ section .text		; start of code segment
 _start:
 
     ; var3 = var1++ + var2
-
+mov rax, [var1]
+add rax, [var2]
+mov [var3], rax
+inc dword [var1]
 
     ; var3--
-
+dec dword [var3]
 
     ; var2 += (var3 - 10)
-
+mov rax, [var3]
+sub rax, 10
+add rax, [var2]
+mov [var2], rax
 
     ; var4 -= 7
-
+mov ax, [var4]
+sub ax, 7
+mov [var4], ax
 
     ; var6 = ++var5 - var4
-
+mov ax, [var5]
+inc ax
+sub ax, [var4]
+mov [var6], ax
 
     ; var9 = -var7 - var8
-
+mov rax, [var7]
+neg rax
+add rax, [var8]
+mov [var9], rax
 
     ; var7 -= ++var8
-
+mov rax, [var8]
+inc rax
+neg rax
+add rax, [var7]
+mov [var7], rax
 
     ; var8 += 7
-
+mov rax, [var8]
+add rax, 7
+mov [var8], rax
 
 	; End the program
 	mov		rax, 0x3c				; system call for exit
